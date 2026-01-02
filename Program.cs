35feb631801ffb9e26f5453b34697a4b37ce63cf
winlogon <walker84837@gmail.com>
2026-01-02 18:27:39 +0100

    chore: remove unneeded using directives

--- Program.cs
+++ Program.cs
@@ -3,9 +3,6 @@ using System.CommandLine.Rendering;
 using HtmlAgilityPack;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace LinguaInRete;
 
